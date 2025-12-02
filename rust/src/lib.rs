9eb6130e1b4056c6c0396e62a537ed8c1c4b1278
Kenny Campbell <Kenny.Campbell@quantinuum.com>
2025-12-02 10:16:38 +0000

    finds_rz_func_defn test working

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,9 +3,24 @@
 // TODO: These docs appear in the landing page of the crate documentation on docs.rs.
 // Make sure to update them to reflect the details of your extension.
 
-use tket::Hugr;
+use tket::hugr::ops::handle::NodeHandle;
+use tket::{Hugr, op_matches};
 use tket::hugr::HugrView;
 use tket::hugr::hugr::hugrmut::HugrMut;
+use tket::hugr::ops::{FuncDefn, OpTag};
+use tket::TketOp;
+
+/// Find the FuncDefn node for the Rz gate
+pub fn find_rz_defn(hugr: &mut Hugr) -> Option<tket::hugr::Node> {
+    for node in hugr.nodes() {
+        let op_type = HugrView::get_optype(hugr, node);
+        if op_matches(op_type, TketOp::Rz) {
+            return Some(node);
+        }
+    }
+    None
+}
+
 
 /// Example function.
 ///
@@ -34,13 +49,18 @@ pub struct ExampleError {
     message: String,
 }
 
+// Test of example function
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    use tket::Hugr;
+    use tket::{Circuit, Hugr};
+    use tket::hugr::builder::{Container, DFGBuilder, Dataflow, HugrBuilder, inout_sig};
+    use tket::hugr::extension::prelude::{qb_t, bool_t};
     use tket::hugr::ops::FuncDefn;
     use tket::hugr::types::Signature;
+    use tket::hugr::ops::Value;
+    use tket::extension::rotation::ConstRotation;
 
     #[test]
     fn it_works() {
@@ -49,5 +69,33 @@ mod tests {
                 .unwrap();
 
         example_remove_contents(&mut hugr).unwrap();
+    }
+
+    #[test]
+    fn finds_rz_func_defn() {
+        let qb_row = vec![qb_t(); 1];
+        let mut h = DFGBuilder::new(Signature::new(qb_row.clone(), qb_row)).unwrap();
+        let [q_in] = h.input_wires_arr();
+
+        let constant = h.add_constant(Value::extension(ConstRotation::PI_2));
+        let loaded_const = h.load_const(&constant);
+        let rz = h.add_dataflow_op(TketOp::Rz, [q_in, loaded_const]).unwrap();
+        let _ = h.set_outputs(rz.outputs());
+        let circ = h.finish_hugr(); //(rz.outputs()).unwrap().into();
+        let defn_node = find_rz_defn(&mut circ.unwrap()).unwrap();
+
+
+        // let mut dfg_builder = DFGBuilder::new(inout_sig(
+        //     vec![qb_t()],
+        //     vec![qb_t(), bool_t()]
+        // )).unwrap();
+        // // Get the wire  
+        // let [wire] = dfg_builder.input_wires_arr();
+
+        // let constant = dfg_builder.add_constant(Value::extension(ConstRotation::PI_2));
+        // let loaded_const = dfg_builder.load_const(&constant);
+        // let rz = dfg_builder.add_dataflow_op(TketOp::Rz, [wire, loaded_const]).unwrap();
+        // let circ = dfg_builder.finish_hugr_with_outputs(rz.outputs()).unwrap().into();
+        
     }
 }
