da5e5cf5ffe810a636630111bf6b4d126828da16
Kenny Campbell <Kenny.Campbell@quantinuum.com>
2025-12-04 16:26:09 +0000

    Updated params for demo

--- rust/examples/demo.rs
+++ rust/examples/demo.rs
@@ -10,11 +10,15 @@ use tket_gridsynth_ext::apply_gridsynth_pass;
 // use tket_gridsynth_ext::lib::apply_gridsynth_pass;
 
 fn main() {
+    // define gridsynth params
+    let theta = 0.64; // Rz angle in radians
+    let epsilon = 1e-2; // desired precision. Gives error if too low.
+
     // Build simple demo hugr containing only Rz gate
     let qb_row = vec![qb_t(); 1];
     let mut h = DFGBuilder::new(Signature::new(qb_row.clone(), qb_row)).unwrap();
     let [q_in] = h.input_wires_arr();
-    let constant = h.add_constant(Value::extension(ConstRotation::PI_4));
+    let constant = h.add_constant(Value::extension(ConstRotation::from_radians(theta).unwrap()));
     let loaded_const = h.load_const(&constant);
     let rz = h.add_dataflow_op(TketOp::Rz, [q_in, loaded_const]).unwrap();
     let _ = h.set_outputs(rz.outputs());
@@ -27,7 +31,6 @@ fn main() {
     println!("{}", hugr.mermaid_string());
 
     // Apply the gridsynth pass
-    let epsilon = 1e-6;
     apply_gridsynth_pass(&mut hugr, epsilon);
 
     // Print result after the gridsynth pass. Paste into a mermaid visualiser to see
